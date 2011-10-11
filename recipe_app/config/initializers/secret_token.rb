f78900769a07cf5497b6e5eca40eec9523fa3de5
Caitlin Johnson <caitlin@cupcait.com>
2011-10-11 12:44:37 -0700

    Initial commit

---
+++ recipe_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecipeApp::Application.config.secret_token = '51e581dcb16249c1f312cb509c0ad6ae1ae31cc28e9da52f71c780676737925c318bce1611f137f9302260623d0b08adc661790a9d5dcf22df94380c40338cd7'
