e7c3d84a1500d19b8766617b64e712171923d676
Caitlin <caitlin@cupcait.com>
2011-10-11 14:57:58 -0700

    Added parameter tests for models

--- recipe_app/app/models/ingredient.rb
+++ recipe_app/app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  attr_accessible :amount, :unit, :name, :preparation
+
+  validates :amount, :presence => true
+  validates :name,   :presence => true
 end
--- recipe_app/app/models/recipe.rb
+++ recipe_app/app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
+  attr_accessible :name, :source, :url, :ingredients, :instructions
+
+  validates :name,         :presence => true
+  validates :source,       :presence => true
+#  validates :ingredients,  :presence => true
+  validates :instructions, :presence => true
+
 end
