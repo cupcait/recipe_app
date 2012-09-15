7b8ee6650bda10002b4078c9da860431fe739df9
Caitlin <caitlin@cupcait.com>
2012-09-14 21:14:25 -0700

    pushing all local changes in order to move laptops...

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,5 @@ class Ingredient < ActiveRecord::Base
   belongs_to :recipe
   attr_accessible :amount, :unit, :name, :preparation, :recipe_id
 
-  validates :amount, :presence => true
   validates :name,   :presence => true
 end
